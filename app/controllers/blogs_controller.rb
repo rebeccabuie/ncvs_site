f4566acc30e02914908b4ab8fdca82febaa7f1b7
rebeccabuie <rebeccabuie@gmail.com>
2017-05-10 17:28:43 -0600

    Added new parameters for user registration and editing account

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -57,6 +57,16 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+
+    redirect_to blogs_url, notice: 'Post status has been updated.'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
