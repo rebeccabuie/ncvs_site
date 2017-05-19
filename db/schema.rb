34a17522cf43b32ea1ca520bb69f705e247f730e
rebeccabuie <rebeccabuie@gmail.com>
2017-05-19 11:19:20 -0600

    Merge problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517224010) do
+ActiveRecord::Schema.define(version: 20170315214224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,12 +50,6 @@ ActiveRecord::Schema.define(version: 20170517224010) do
     t.integer  "position"
   end
 
-  create_table "topics", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -75,5 +69,4 @@ ActiveRecord::Schema.define(version: 20170517224010) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "blogs", "topics"
 end
