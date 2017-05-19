ae5ef389186af94510dbe9cfa06e54cfc11a3178
rebeccabuie <rebeccabuie@gmail.com>
2017-05-18 20:36:38 -0600

    Implemented gritter for notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121200049) do
+ActiveRecord::Schema.define(version: 20170517224010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,5 +76,4 @@ ActiveRecord::Schema.define(version: 20170121200049) do
   end
 
   add_foreign_key "blogs", "topics"
-  add_foreign_key "technologies", "portfolios"
 end
