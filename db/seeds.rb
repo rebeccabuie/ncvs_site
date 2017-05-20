9ea9539e0707bfe00af8bdb68337190783a37f99
rebeccabuie <rebeccabuie@gmail.com>
2017-05-19 19:16:37 -0600

    Pushing a migrate debugging process that didn't work so I'm going back to see what I missed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,3 @@
-User.create!(
-  email: "admint@test.com",
-  password: "asdfasdf",
-  password_confirmation: "asdfasdf",
-  name: "Admin User",
-  roles: "root_admin"
-)
-
-puts "1 Admin user created"
-
-User.create!(
-  email: "test2@test.com",
-  password: "asdfasdf",
-  password_confirmation: "asdfasdf",
-  name: "Regular User",
-)
-
-puts "1 regular user created"
-
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -27,20 +8,10 @@ end
 
 puts "10 blog posts created"
 
-8.times do |portfolio_item|
+9.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Ruby on Rails",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200"
-  )
-end
-
-1.times do |portfolio_item|
-  Portfolio.create!(
-    title: "Portfolio title: #{portfolio_item}",
-    subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
