a8f1e39a002e7154e2618552383db7197142214f
rebeccabuie <rebeccabuie@gmail.com>
2017-11-30 18:44:52 -0700

    almost done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,4 @@
 module ApplicationHelper
-  def login_helper style = ''
-    if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
-    else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
-    end
-  end
-
-  def source_helper(styles)
-    if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path } if you'd like to work together."
-      content_tag(:div, greeting.html_safe, class: styles)
-    end
-  end
 
   def nav_items
     [
